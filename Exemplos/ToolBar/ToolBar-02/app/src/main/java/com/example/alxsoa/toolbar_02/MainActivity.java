4391ccf8b9e8a4a95a90ddf26152988364dbe650
Alexandre Soares <alexandre.asoares@gmail.com>
2015-03-17 08:08:50 -0300

    17.03.2015
    Templates de ToolBar

---
+++ Exemplos/ToolBar/ToolBar-02/app/src/main/java/com/example/alxsoa/toolbar_02/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.alxsoa.toolbar_02;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends ActionBarActivity
+{
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+        if (id == R.id.action_settings)
+        {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
