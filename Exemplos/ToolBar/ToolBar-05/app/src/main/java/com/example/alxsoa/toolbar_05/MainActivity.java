e29af4a9b1e876a6d77f880b85fe1bcfeaad6a17
Alexandre Soares <alexandre.asoares@gmail.com>
2015-03-22 12:41:24 -0300

    22.03.2015

---
+++ Exemplos/ToolBar/ToolBar-05/app/src/main/java/com/example/alxsoa/toolbar_05/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.alxsoa.toolbar_05;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends ActionBarActivity
+{
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+        if (id == R.id.action_settings)
+        {
+            return true;
+        }
+
+        if (id==R.id.navigate)
+        {
+            startActivity (new Intent(this, SubActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
