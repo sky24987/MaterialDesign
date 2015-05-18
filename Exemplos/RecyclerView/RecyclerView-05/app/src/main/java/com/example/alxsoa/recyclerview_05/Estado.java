1aeb6ae410c6d38fc2fb0970227cb34855c57e5c
Alexandre Soares <alexandre.asoares@gmail.com>
2015-05-18 19:20:20 -0300

    18052015.1

---
+++ Exemplos/RecyclerView/RecyclerView-05/app/src/main/java/com/example/alxsoa/recyclerview_05/Estado.java
@@ -0,0 +1,47 @@
+package com.example.alxsoa.recyclerview_05;
+
+public class Estado
+{
+    private String strEstado;
+    private String strCapital;
+    private int iBandeira;
+
+    public Estado(){}
+    public Estado(String strEstadoAux, String strCapitalAux, int iBandeiraAux)
+    {
+        strEstado = strEstadoAux;
+        strCapital = strCapitalAux;
+        iBandeira = iBandeiraAux;
+    }
+
+
+    public String getStrEstado()
+    {
+        return strEstado;
+    }
+
+    public void setStrEstado(String strEstado)
+    {
+        this.strEstado = strEstado;
+    }
+
+    public String getStrCapital()
+    {
+        return strCapital;
+    }
+
+    public void setStrCapital(String strCapital)
+    {
+        this.strCapital = strCapital;
+    }
+
+    public int getBandeira ()
+    {
+        return iBandeira;
+    }
+
+    public void setBandeira ( int iBandeira )
+    {
+        this.iBandeira = iBandeira;
+    }
+}
---
+++ Exemplos/RecyclerView/RecyclerView-05/app/src/main/java/com/example/alxsoa/recyclerview_05/RecyclerViewOnClickListenerHack.java
@@ -0,0 +1,9 @@
+package com.example.alxsoa.recyclerview_05;
+
+import android.view.View;
+
+public interface RecyclerViewOnClickListenerHack
+{
+    public void onClickListener(View view, int position);
+    public void onLongPressClickListener(View view, int position);
+}
